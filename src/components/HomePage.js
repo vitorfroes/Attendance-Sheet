9a07d21979c5c76c3cc54e22e5f56f52853d8d4a
Vitor Barbosa Fróes Souza <vitor.souza@dte.local>
2020-02-17 15:25:37 -0300

    configure nav to a NavLink

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const HomePage = props => {
   return (
     <div className="jumbotron">
       <h1>Attendance Sheet</h1>
       <p>EBD Classes</p>
+
       <hr className="my-4"></hr>
-      <a href="/about">About us</a>
+
+      <Link to="about" className="btn btn-primary">
+        About
+      </Link>
     </div>
   );
 };
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,36 +1,43 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
+  const activeStyle = { color: "#52cc72" };
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">
+      <NavLink className="navbar-brand" to="#">
         <img
           src="https://img.icons8.com/plasticine/2x/list.png"
           width="50"
           alt="list-icon"
         ></img>
         Attendance Sheet
-      </a>
+      </NavLink>
       <ul className="nav">
         <li className="nav-item">
-          <a href="/" className="nav-link">
+          <NavLink to="/" activeStyle={activeStyle} exact className="nav-link">
             Home
-          </a>
+          </NavLink>
         </li>
         <li className="nav-item">
-          <a href="/students" className="nav-link">
+          <NavLink
+            to="/students"
+            activeStyle={activeStyle}
+            className="nav-link"
+          >
             Students
-          </a>
+          </NavLink>
         </li>
         <li className="nav-item">
-          <a href="/classes" className="nav-link">
+          <NavLink to="/classes" activeStyle={activeStyle} className="nav-link">
             Classes
-          </a>
+          </NavLink>
         </li>
         <li className="nav-item">
-          <a href="/about" className="nav-link">
+          <NavLink to="/about" activeStyle={activeStyle} className="nav-link">
             About
-          </a>
+          </NavLink>
         </li>
       </ul>
     </nav>
